de99229a80567a56e4614fcafde9e02609f0a4f4
Mathsqrt2 <micho.bugajski@gmail.com>
2024-02-13 20:57:16 +0100

    first launch detections fix, OS detection, fix path function implemented

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -1,4 +1,48 @@
+var proj = app.project;
+var seq = proj.activeSequence;
+var currentOS;
+
 $.processEffects = {
-    proj = app.project,
-    seq = proj.activeSequence,
+    fixPath: function(pathToFix) {
+        var newPath = pathToFix;
+        if (!currentOS) {
+            while (newPath.indexOf("/") > 0) {
+                newPath = newPath.replace('/', '\\');
+            }
+            return newPath;
+        } else {
+            while (newPath.indexOf("\\") > 0) {
+                newPath = newPath.replace("\\", "/");
+            }
+            return newPath;
+        }
+    },
+}
+
+function setOSValue(csinfo) {
+    var obj = JSON.parse(csinfo);
+    currentOS = obj.index;
+}
+
+function isItFirstUseJSX(path) {
+    var newResponse = {
+        isItFirstUse: true,
+        actionTime: null,
+    };
+
+    var logPath = $.processEffects.fixPath(path) + $.processEffects.fixPath("\\logs\\firstLaunchLog.json");
+    var firstLaunchLog = new File(logPath);
+
+    if (firstLaunchLog.exists) {
+        newResponse.isItFirstUse = false;
+    } else {
+        var currentTime = new Date();
+        newResponse.actionTime = currentTime.getTime();
+        firstLaunchLog.open("w");
+        firstLaunchLog.write(JSON.stringify(newResponse));
+        firstLaunchLog.close();
+    }
+
+    var output = JSON.stringify(newResponse);
+    return output;
 }
